7adaba05831690e06adbc2f88762df978cee15a0
danuaf <99816691+danuaf@users.noreply.github.com>
2026-06-11 10:27:54 +0700

    first commit

---
+++ src/test/java/com/indivaragroup/jdt17/spring/rest/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indivaragroup.jdt17.spring.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
